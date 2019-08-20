1b87ac3248a6f530a5700f7a0b3a0d4b360c4cdb
Johnson Joseph <jhonsonj88@gmail.com>
2019-08-20 15:47:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.querySelector( '#nested .target')
 }
+
+function increaseRankBy(n){
+    let list = document.querySelectorAll( '.ranked-list' )
+
+    for (let i = 0; i < list.length; i++) {
+        let secondList = list[i].children
+
+        for(let j = 0; j < secondList.length; j++){ 
+           secondList[j].innerHTML = (parseInt(secondList[j].innerHTML) + n).toString()
+        }
+      }
+}
